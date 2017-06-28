ee090f33a8b30cd704876f9b6eb88510a6ec7ed2
evankielley <evankielley@gmail.com>
2017-06-28 18:21:13 -0230

    added commments and cleaned code

--- WagnerModel/Model/analytic_parameters.m
+++ WagnerModel/Model/analytic_parameters.m
@@ -5,7 +5,9 @@
 %
 % Till Wagner, Oct 2016, tjwagner@ucsd.edu
 % -------------------------------------------------------------------------
-R = 6378*1e3;           %earth radius in m
+
+% set constants -----------------------------------------------------------
+R = 6378*1e3;           % earth radius in m
 rhow = 1027;            % density of water (kg/m^3)
 rhoa = 1.2;             % density of air   (kg/m^3)
 rhoi = 850;             % density of ice   (kg/m^3)
@@ -13,22 +15,23 @@ drho = rhow-rhoi;
 Cw = 0.9;               % bulk coefficient water  (Bigg et al 1997)
 Ca = 1.3;               % bulk coefficient air    (Bigg et al 1997)
 om = 7.2921*1e-5;       % rotation rate of earth (rad/s)
+
+% define necessary functions ----------------------------------------------
 ff = @(lati) 2*om*sin(abs(lati)*pi/180);        % latitude in degrees
 g = sqrt(rhoa*drho/rhow/rhoi*(Ca/Cw));          % gamma = sqrt(ca/cw)
 S = @(l,w) pi*l.*w./(l+w);                      % harmonic mean length
-Ut = @(u,lati,S) sqrt(2)*Cw*g/ff(lati).*u/S;    % \Lambda in the papers
-% -------------------------------------------------------------------------
-% the following are slightly differently non-dimmed in the papers (sqrt(2))
-% need to make those consistent at some point -----------------------------
-a = @(U) sqrt(2)./U.^3.*(1-sqrt(1+U.^4));       
+Ut = @(u,lati,S) sqrt(2)*Cw*g/ff(lati).*u/S;    % Lambda in the papers
+
+% alpha and beta functions
+% these are slightly differently in the papers (sqrt(2)) -- not sure why
+% b_big is accurate for U greater than 0.1 otherwise b_small is to be used
+a = @(U) sqrt(2)./U.^3.*(1-sqrt(1+U.^4));
 b_big = @(U) real(1./U.^3.*sqrt((4+U.^4).*sqrt(1+U.^4)-3*U.^4-4));
 b_small = @(U) real(U.^3./sqrt(8).*sqrt(1-3.*U.^4./4+9.*U.^8./16-7.*U.^12./16+45.*U.^16./256));
-%b_small = @(U) real(1./U.^3.*sqrt(U.^12./8-3.*U.^16/32+9*U^20/128-7*U^24/128+45*U^28/1024));
-% Melt parameters (WDE16 Appendix)-----------------------------------------
-% -------------------------------------------------------------------------
+
+% Melt parameters (WDE16 Appendix) ----------------------------------------
 Ti0 = -4;
 Cs1 = 1.5; Cs2 = 0.5; Cs3 = 0.1;
 CMv1 = 7.62e-3; CMv2 = 1.29e-3;
 CMe1 = 0.5;
 CMb1 = 0.58; CMb2 = 0.8; CMb3 = 0.2;
-% -------------------------------------------------------------------------
--- WagnerModel/Model/melting.m
+++ WagnerModel/Model/melting.m
@@ -6,43 +6,54 @@
 %
 % Till Wagner, Oct 2016, tjwagner@ucsd.edu
 % -------------------------------------------------------------------------
+
+% set dummy iterator ------------------------------------------------------
 I = i; 
+
+% initialize melt output vector ----------------------------------------
 mmelted(I)=false;
+
 % Compute Melt Terms-------------------------------------------------------
 Me = CMe1*(Cs1*Ua^Cs2 + Cs3*Ua);      % wind driven erosion
 Mv = CMv1*SST + CMv2*SST^2;           % thermal side wall erosion
 Mb = CMb1*sqrt((ui - uw).^2 + (vi - vw).^2)^CMb2...
     *(SST-Ti0)/l(I)^CMb3;             % turbulent basal melt
+
 % Apply Melt Rates --------------------------------------------------------
 dldt = - Mv - Me; dhdt = - Mb;
 l(I+1) = l(I) + dldt*Dt; 
 w(I+1) = w(I)+dldt*Dt; 
 h(I+1)=h(I)+dhdt*Dt;
-% -------------------------------------------------------------------------
-% Make sure the berg is not negative size
+
+% Make sure the berg is not negative size ---------------------------------
 if l(I+1)<0 || w(I+1)<0 || h(I+1)<0
     l(I+1)=0; w(I+1)=0; h(I+1)=0;
     melted = 1;
     mmelted(I)=true;
     mm = mm+1;
 end
-% Rollovers:
+
+% Check if the iceberg rolled over ----------------------------------------
 if w(I+1) < 0.85 * h(I+1)
     hn = w(I+1); w(I+1) = h(I+1); h(I+1) = hn;
 end
-% Make sure length>width:
+
+% Make sure length is greater than width ----------------------------------
 if w(I+1)>l(I+1)
     wn = l(I+1); l(I+1)=w(I+1); w(I+1) = wn;
 end
-% save length, width, and height
+
+% save length, width, and height ------------------------------------------
 ml(I+1)=l(I+1); mw(I+1)=w(I+1);mh(I+1)=h(I+1);
 
-% Compute new volume and dvol
+% Compute new volume and dvol ---------------------------------------------
 v(I+1) = l(I+1)*w(I+1)*h(I+1);
 dv(I+1) = v(I)-v(I+1);
-% Check whether iceberg survived-------------------------------------------
+
+% Check whether iceberg survived ------------------------------------------
 if I == lt-1 && v(I+1) > 0
     ss = ss+1;
 end
-% -------------------------------------------------------------------------
-Mev(I) = Me; Mvv(I) = Mv; Mbv(I) = Mb;  %store melt rates
+
+% store melt rates --------------------------------------------------------
+Mev(I) = Me; Mvv(I) = Mv; Mbv(I) = Mb;  
