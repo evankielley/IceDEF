983840a16070387b63bce9e5a38fb204c60dad54
evankielley <evankielley@gmail.com>
2018-03-15 19:18:44 -0230

    moving two directories, models and plotting, into a directory called source

---
+++ source/models/drift/wagner.py
@@ -0,0 +1,132 @@
+"""Iceberg drift model from Wagner, 2017.
+.. module:: wagner
+    :platform: Unix, Windows
+    :synopsis: This module computes the trajectory of an iceberg in terms of it's longitude and latitude. The equations used in this model stem from the paper, An Analytical Model of Iceberg Drift, Wagner et. al. (2017).
+.. moduleauthor:: Evan Kielley <evankielley@gmail.com>
+"""
+
+import numpy as np
+import cmath
+
+# Constants
+R = 6378*1e3
+om = 7.2921e-5
+rhow = 1027
+rhoa = 1.2
+rhoi = 850
+drho = rhow - rhoi
+Cw = 0.9
+Ca = 1.3
+gam = np.sqrt(rhoa*drho/rhow/rhoi*(Ca/Cw))
+sst0 = -4
+Cs1 = 1.5; Cs2 = 0.5; Cs3 = 0.1
+CMv1 = 7.62e-3; CMv2 = 1.29e-3; CMe1 = 0.5
+CMb1 = 0.58; CMb2 = 0.8; CMb3 = 0.2
+
+
+
+def wagner_drift(x, y, l, w, h, UA, VA, UW, VW, SST, t_ocean, t_atm, dt):
+    """This functions computes the trajectory of an iceberg over one timestep.
+    .. function:: wagner_drift
+        :param x: The iceberg's longitudinal coordinate (degrees).
+        :type x: float.
+        :param y: The iceberg's latitudinal coordinate (degrees).
+        :type y: float.
+        :param l: The iceberg's length dimension (m).
+        :type l: float.
+        :param w: The iceberg's width dimension (m).
+        :type w: float.
+        :param h: The iceberg's height dimension (m).
+        :type h: float.
+        :param UA: The interpolated u-component of the wind velocity (m/s), East is positive.
+        :type UA: array_like, shape (t, x, y).
+        :param VA: The interpolated v-component of the wind velocity (m/s), North is positive.
+        :type VA: array_like, shape (t, x, y).
+        :param UW: The interpolated u-component of the current velocity (m/s), East is positive.
+        :type UW: array_like, shape (t, x, y).
+        :param VW: The interpolated v-component of the current velocity (m/s), North is positive.
+        :type VW: array_like, shape (t, x, y).
+        :param SST: The interpolated sea-surface temperature (degrees C).
+        :type SST: array_like, shape (t, x, y).
+        :param t_ocean: The timestep that agrees with the time units for the ocean current field.
+        :type t_ocean: float.
+        :param t_ocean: The timestep that agrees with the time units for the wind field.
+        :type t_atm: float.
+        :param dt: The timestep length (s).
+        :type dt: float.
+        :return x_new: The new longitudinal position of the iceberg (degrees).
+        :rtype x_new: float.
+        :return y_new: The new latitudinal position of the iceberg (degrees).
+        :rtype y_new: float.
+        :return l_new: The new length of the iceberg (m).
+        :rtype l_new: float.
+        :return w_new: The new width of the iceberg (m).
+        :rtype w_new: float.
+        :return h_new: The new height of the iceberg (m).
+        :rtype h_new: float.
+    """
+
+
+    # Extract values from input fields
+    
+    vau = UA([t_atm, y, x])[0]
+    vav = VA([t_atm, y, x])[0]  
+    vwu = UW([t_ocean, y, x])[0] 
+    vwv = VW([t_ocean, y, x])[0]
+    sst = SST([t_ocean, y, x])[0]
+    
+    
+    # Drift
+
+    S = np.pi*((l*w)/(l+w))
+    ff = 2*om*np.sin((np.abs(y)*np.pi)/180)
+    lam = np.sqrt(2)*Cw*(gam*np.sqrt(vau**2 + vav**2))/(ff*S)
+
+    
+    if lam < 0.1:
+        alpha = lam*(lam**4*(lam**4*(lam**4*(-0.0386699020961393*lam**4 + \
+            0.055242717280199) - 0.0883883476483184) + \
+            0.176776695296637) - 0.707106781186548)
+    else:
+        alpha = np.multiply(np.divide(np.sqrt(2),np.power(lam, 3)),(1-np.sqrt(1+np.power(lam,4))))
+        
+    if lam < 0.6:
+        beta = lam**3*(lam**4*(lam**4*(lam**4*(lam**4*(lam**4*(lam**4*(lam**4*(lam**4*\
+            (0.0153268598203613*lam**4 - 0.0151656272365985) + \
+            0.0180267866272764) + 0.0219176256311202) - \
+            0.0274446790511418) + 0.0357675015202851) - \
+            0.0493731785691779) + 0.0745776683282687) - \
+            0.132582521472478) + 0.353553390593274)
+    else:
+        beta = np.real(np.multiply(np.divide(1.,np.power(lam,3.)),cmath.sqrt(np.multiply((4.+np.power(lam,4.)), \
+            cmath.sqrt(1.+np.power(lam,4.)))-3.*np.power(lam,4.)-4.)))
+
+        
+    viu = vwu + gam*(-alpha*vav + beta*vau)
+    viv = vwv + gam*(alpha*vau + beta*vav)
+
+    y_new = y + (viv*dt)*(180/(np.pi*R))
+    x_new = x + (viu*dt)/(np.cos((((y + y_new)/2)*np.pi)/180))*(180/(np.pi*R))
+
+
+    # Decay
+
+    Me = CMe1*(Cs1*np.sqrt(vau**2 + vav**2)**Cs2 + Cs3*np.sqrt(vau**2 + vav**2))
+    Mv = CMv1*sst + CMv2*sst**2
+    Mb = CMb1*np.power(np.sqrt(np.square(viu-vwu)+np.square(viv-vwv)),CMb2)*(sst - sst0)/l**CMb3
+
+    l_new = l - (Mv + Me)*(dt/(24*3600))  # convert dt from secs to days
+    w_new = w - (Mv + Me)*(dt/(24*3600))
+    h_new = h - Mb*(dt/(24*3600))
+
+    if w_new < 0.85*h_new:
+        # Rollover
+        print('rollover')
+        w_new, h_new = h_new, w_new
+
+    if w_new > l_new:
+        # Ensure l is greater than w
+        print('swap l and w')
+        w_new, l_new = l_new, w_new
+        
+    return x_new, y_new, l_new, w_new, h_new
---
+++ source/models/metocean/metocean.py
@@ -0,0 +1,122 @@
+import scipy.interpolate as interp
+import datetime
+from datetime import date, timedelta
+import urllib
+import netCDF4 as nc
+import numpy as np
+
+
+class Metocean(object):
+
+    def __init__(self, x_min, x_max, y_min, y_max, t_min, t_max):
+        self.x_min = x_min - abs(x_min-x_max) - self.xy_res
+        self.x_max = x_max + abs(x_min-x_max) + self.xy_res
+        self.y_min = y_min - abs(y_min-y_max) - self.xy_res
+        self.y_max = y_max + abs(y_min-y_max) + self.xy_res
+        self.t_min = t_min - timedelta(hours = self.t_res)
+        self.t_max = t_max + timedelta(hours = self.t_res)
+
+                                           
+    
+    def convert_datetime2time(self, t, t_units, t_calendar, t_offset=0):
+                                                
+        dt += timedelta(hours = t_offset)
+        t = nc.date2num(t, t_units, t_calendar)
+        
+        return t
+
+
+
+class ECMWF_Ocean(Metocean):
+    
+    # product identifier: GLOBAL_ANALYSIS_FORECAST_PHY_001_024
+
+    path = 'ftp://data.munroelab.ca/pub/ECMWF/ocean/daily/'
+    xy_res = 1/12  # spatial resolution in degrees lat/lon
+    t_res = 1  # temporal resolution in hours
+    t_units = 'hours since 1950-01-01 00:00:00'
+    t_calendar = 'standard'
+    
+    def __init__(self, x_min, x_max, y_min, y_max, t_min, t_max):
+        
+        super().__init__(x_min, x_max, y_min, y_max, t_min, t_max)
+        
+        self.filenames, self.files = self.get_filenames(self.t_min, self.t_max, self.path)
+        self.ds = nc.MFDataset(self.files)
+        self.times = self.ds.variables['time'][:]
+        self.datetimes = nc.num2date(self.times, self.t_units, self.t_calendar)
+        self.t1900 = nc.date2num(self.datetimes, 'hours since 1900-01-01 00:00:00.0 00:00', 'standard')
+        self.t1950 = nc.date2num(self.datetimes, 'hours since 1950-01-01 00:00:00.0 00:00', 'standard')
+        self.t2000 = nc.date2num(self.datetimes, 'hours since 2000-01-01 00:00:00.0 00:00', 'standard')
+        self.lats = self.ds.variables['latitude'][:]
+        self.lons = self.ds.variables['longitude'][:]
+        self.UW = np.asarray(self.ds.variables['uo'][:,0,:,:])
+        self.VW = np.asarray(self.ds.variables['vo'][:,0,:,:])
+        self.SST = np.asarray(self.ds.variables['thetao'][:,0,:,:])
+        self.iUW = interp.RegularGridInterpolator((self.times, self.lats, self.lons), self.UW)
+        self.iVW = interp.RegularGridInterpolator((self.times, self.lats, self.lons), self.VW)
+        self.iSST = interp.RegularGridInterpolator((self.times, self.lats, self.lons), self.SST)
+
+
+    
+    def get_filenames(self, t_min, t_max, path):
+
+        d1 = date(self.t_min.year, self.t_min.month, self.t_min.day)  # start date
+        d2 = date(self.t_max.year, self.t_max.month, self.t_max.day)  # end date
+        delta = d2 - d1  # timedelta
+
+        filenames = []
+        files = []
+
+        for i in range(delta.days + 1):
+            new_date = d1 + timedelta(days=i)
+            filenames.append(path + str(new_date).replace('-', '') + '.nc')
+            files.append(urllib.request.urlretrieve(filenames[i])[0])
+
+        return filenames, files
+
+
+
+class ECMWF_Atm(Metocean):
+    
+    # product identifier: WIND_GLO_WIND_L4_NRT_OBSERVATIONS_012_004
+    path = 'ftp://data.munroelab.ca/pub/ECMWF/atm/daily/'
+    xy_res = 1/4  # spatial resolution in degrees lat/lon
+    t_res = 6  # temporal resolution in hours
+    t_units = 'hours since 1900-01-01 00:00:00.0 00:00'
+    t_calendar = 'standard'
+    
+    def __init__(self, x_min, x_max, y_min, y_max, t_min, t_max):
+        
+        super().__init__(x_min, x_max, y_min, y_max, t_min, t_max)
+        
+        self.filenames, self.files = self.get_filenames(self.t_min, self.t_max, self.path)
+        self.ds = nc.MFDataset(self.files)
+        self.times = self.ds.variables['time'][:]
+        self.datetimes = nc.num2date(self.times, self.t_units, self.t_calendar)
+        self.t1900 = nc.date2num(self.datetimes, 'hours since 1900-01-01 00:00:00.0 00:00', 'standard')
+        self.t1950 = nc.date2num(self.datetimes, 'hours since 1950-01-01 00:00:00.0 00:00', 'standard')
+        self.t2000 = nc.date2num(self.datetimes, 'hours since 2000-01-01 00:00:00.0 00:00', 'standard')
+        self.lats = self.ds.variables['latitude'][:]
+        self.lons = self.ds.variables['longitude'][:]
+        self.UA = np.asarray(self.ds.variables['eastward_wind'][:,0,:,:])
+        self.VA = np.asarray(self.ds.variables['northward_wind'][:,0,:,:])
+        self.iUA = interp.RegularGridInterpolator((self.times, self.lats, self.lons), self.UA)
+        self.iVA = interp.RegularGridInterpolator((self.times, self.lats, self.lons), self.VA)
+        
+
+    def get_filenames(self, t_min, t_max, path):
+
+        d1 = date(self.t_min.year, self.t_min.month, self.t_min.day)  # start date
+        d2 = date(self.t_max.year, self.t_max.month, self.t_max.day)  # end date
+        delta = d2 - d1  # timedelta
+
+        filenames = []
+        files = []
+
+        for i in range(delta.days + 1):
+            new_date = d1 + timedelta(days=i)
+            filenames.append(path + 'sub' + str(new_date).replace('-', '') + '.nc')
+            files.append(urllib.request.urlretrieve(filenames[i])[0])
+
+        return filenames, files
