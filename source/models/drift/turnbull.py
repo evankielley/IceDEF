3aee03c6a8ea6069cbc420eb9c9cfdefe929426f
evankielley <evankielley@gmail.com>
2018-03-19 16:06:11 -0230

    added skin drag and some keel and sail info

--- source/models/drift/turnbull.py
+++ source/models/drift/turnbull.py
@@ -11,7 +11,6 @@ def turnbull_drift(t, dt, vx, vy, x, y, l, w, h, UA, VA, UW, VW):
     Vy = vy
         
 
-    
     # calculate air density
     # TODO: effect of temperature and humidity
     rhoa = 1.225 # density of air (kg/m^3)
@@ -19,31 +18,26 @@ def turnbull_drift(t, dt, vx, vy, x, y, l, w, h, UA, VA, UW, VW):
     rhoi = 900
     Ca = 1.2e-3 #+ 0.1)/2  # air drag coefficient
     Cw =  0.5 #(5.5e-3+ 2.5)/2    # water drag coefficient
-    Cdw = 5.0e-4
     
     # Keel info
+    Cdw = 5.0e-4  # skin drag in water coefficient
     keel_shape = 'triangular'
     #keel_shape = 'rectangular'
-    #keel_shape = 'max_rectangular'
+    Ab = 0
 
-    
     if keel_shape is 'triangular':
-        Ak = (rhoi/rhow)*(2/np.pi)*(l+w)*h / 2  # from Wagner
-        As = ((rhow - rhoi)/rhoi)*Ak / 2   # from Wagner   
-    
+        Ak = (rhoi/rhow)*(2/np.pi)*(l+w)*h / 2  # from Wagner   
     elif keel_shape is 'rectangular':
-        Ak = (rhoi/rhow)*(2/np.pi)*(l+w)*h  # from Wagner
-        As = ((rhow - rhoi)/rhoi)*Ak   # from Wagner
-        
-    elif keel_shape is 'max_rectangular':
-        hk = (rhoi/rhow)*h  # max keel height
-        hs = h - hk  # max sail height
-        Ak = l*hk  # max keel area
-        As = l*hs  # max sail area 
-        
+        Ak = (rhoi/rhow)*(2/np.pi)*(l+w)*h  # from Wagner      
     else:
         print('Invalid keel shape')
-
+        
+    
+    # Sail info
+    Cda = 2.5e-4  # skin drag in air coefficient (Lichey and Hellmer, 2001).
+    sail_shape = 'rectangular'
+    As = ((rhow - rhoi)/rhoi)*Ak / 2   # from Wagner   
+    At = l*w  # area of top of sail
     
     om = 7.2921e-5  # rotation rate of earth in rad/s
     f = 2*om*np.sin(np.deg2rad(y))  # Coriolis parameter (y is latitude in degrees)
@@ -53,28 +47,21 @@ def turnbull_drift(t, dt, vx, vy, x, y, l, w, h, UA, VA, UW, VW):
     
     earth_radius = 6378*1e3
 
+    
+    # Air force
+    Fax = (0.5*rhoa*Ca*As + rhoa*Cda*At)*abs(Vax - Vx)*(Vax - Vx)
+    Fay = (0.5*rhoa*Ca*As + rhoa*Cda*At)*abs(Vay - Vy)*(Vay - Vy)
+    
 
-    
-    #As = l * 0.1*h  # sail area (maximum width (m) * sail height (m))
-    
-    # TODO: add skin drag
-    Fax = (0.5 * rhoa * Ca * As) * abs(Vax - Vx) * (Vax - Vx)
-    Fay = (0.5 * rhoa * Ca * As) * abs(Vay - Vy) * (Vay - Vy)
-       
-    
-    #Ak = 7*110    # keel area (width (m) * keel depth (m))
-    #Ak = l * 0.9*h
-    
-    
+    # Water force
     # TODO: extend to n-layer keel model
-    # TODO: add skin drag
-    Fwx = (0.5 * rhow * Cw * Ak *abs(Vcx - Vx)*(Vcx-Vx)) +(0)
-    Fwy = (0.5 * rhow * Cw * Ak *abs(Vcy - Vy)*(Vcy-Vy)) +(0)
+    Fwx = (0.5*rhow*Cw*Ak  + rhow*Cdw*Ab)*abs(Vcx - Vx)*(Vcx - Vx) 
+    Fwy = (0.5*rhow*Cw*Ak + rhow*Cdw*Ab)*abs(Vcy - Vy)*(Vcy - Vy)
     
     
     # Coriolis force
-    Fcx = + f * Vy * berg_mass
-    Fcy = - f * Vx * berg_mass
+    Fcx = f*Vy*berg_mass
+    Fcy = -f*Vx*berg_mass
     
     
     # Water pressure gradient
@@ -90,16 +77,15 @@ def turnbull_drift(t, dt, vx, vy, x, y, l, w, h, UA, VA, UW, VW):
     Fwpy = berg_mass*(Amwy - f*Vwmy)
     
     
-    # Iceberg acceleration
+    # Iceberg acceleration                
+    ax = (Fax + Fcx + Fwx + Fwpx)/(berg_mass + Ma)
+    ay = (Fay + Fcy + Fwy + Fwpy)/(berg_mass + Ma)
     
-    # added mass
-                
-    ax = (Fax + Fcx + Fwx + Fwpx) / (berg_mass + Ma)
-    ay = (Fay + Fcy + Fwy + Fwpy) / (berg_mass + Ma)
-    
+    # Iceberg velocity
     vx += dt*ax
     vy += dt*ay
     
+    # Iceberg position
     y_new = y + dt*vy*(180/(np.pi*earth_radius))
     x_new = x + dt*vx/(np.cos((((y + y_new)/2)*np.pi)/180))*(180/(np.pi*earth_radius))
     
