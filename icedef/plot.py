e83d00566736921fbc18dcbbbf37dac042ed3c2b
evankielley <evankielley@gmail.com>
2018-05-25 15:30:45 -0230

    added documentation, removed deprecated plot functions, and added a new plot function that plot a drift test case (i.e a simulated iceberg alongside its observed IIP data points).

--- icedef/plot.py
+++ icedef/plot.py
@@ -1,261 +1,90 @@
+"""This module makes plots and animations for objects in the icedef package.
+"""
+
 import matplotlib.pyplot as plt
-import numpy as np
-import cartopy.crs as ccrs
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 from mpl_toolkits.basemap import Basemap
-import bisect
 from matplotlib.animation import FuncAnimation
+import numpy as np
 import netCDF4 as nc
 
 
-def plot0(iip_berg, mod_berg):
-    tol = 0.1  # tstep
-    for i,t in enumerate(iip_berg.t2000):
-        diff = abs(np.asarray(mod_berg.t2000) - t)
-        for j,diff_t in enumerate(diff):
-            if diff_t < tol:
+def plot_drift_track_test_case(iip_berg, mod_berg):
+    """This function plots the drift track of a simulated iceberg against its observed coordinates"
+    
+    Args:
+        iip_berg (Iceberg): all attributes correspond to data from a particular IIP iceberg.
+        mod_berg (Iceberg): initial attributes and final time correspond to data from a particular 
+                            IIP iceberg but the rest comes from drift simulation.  
+    """
+    
+    buff = 0.1
+
+    berg_xmin = min(min(mod_berg.history['X']), min(iip_berg.history['X'])) 
+    berg_xmax = max(max(mod_berg.history['X']), max(iip_berg.history['X']))
+    berg_ymin = min(min(mod_berg.history['Y']), min(iip_berg.history['Y']))
+    berg_ymax = max(max(mod_berg.history['Y']), max(iip_berg.history['Y']))
+
+
+    m = Basemap(projection='merc', lat_0 = 57, lon_0 = -135,
+                resolution = 'l', area_thresh = 0.1,
+                llcrnrlon = berg_xmin - buff, 
+                llcrnrlat = berg_ymin - buff,
+                urcrnrlon = berg_xmax + buff, 
+                urcrnrlat = berg_ymax + buff)
+
+    m.drawcoastlines()
+    m.drawcountries()
+    m.fillcontinents(color = 'coral')
+    m.drawmapboundary()
+
+    mod_lons = mod_berg.history['X']
+    mod_lats = mod_berg.history['Y']
+    mod_x, mod_y = m(mod_lons, mod_lats)
+    m.plot(mod_x, mod_y, 'bo', markersize=1)
+
+    iip_lons = iip_berg.history['X']
+    iip_lats = iip_berg.history['Y']
+    iip_x, iip_y = m(iip_lons, iip_lats)
+    m.plot(iip_x, iip_y, marker='X', color='black', markersize=5)
+
+    mod_times = mod_berg.history['T']
+    
+    mod_dtimedelta = mod_times[1] - mod_times[0]
+    matching_arr = []
+    for iip_dtime in iip_berg.history['T']:
+        for mod_index, mod_dtime in enumerate(mod_berg.history['T']):
+            dtimedelta = abs(iip_dtime - mod_dtime)
+            if dtimedelta < mod_dtimedelta:
+                matching_arr.append(mod_index)
                 break
-        plt.plot(iip_berg.lats[i], iip_berg.lons[i], marker='o', color='red')
-        plt.text(iip_berg.lats[i], iip_berg.lons[i], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-        plt.plot(mod_berg.lats[j], mod_berg.lons[j], marker='o', color='yellow')
-        plt.text(mod_berg.lats[j], mod_berg.lons[j], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-    plt.plot(iip_berg.lats, iip_berg.lons, label='observed', color='red')
-    plt.plot(mod_berg.lats, mod_berg.lons, label='computed', color='orange')
-    plt.legend()
-    plt.xlabel('Latitude'); plt.ylabel('Longitude')
-    plt.title('Iceberg: {}\nStart time: {}'.format(iip_berg.id_num, iip_berg.times[0]))
-    i = 0
-    filename = './drift_track_{}'.format(iip_berg.id_num)
-    i = 0
-    while True:
-        i += 1
-        newname = '{}_{:d}.png'.format(filename, i)
-        if os.path.exists(newname):
-            continue
-        plt.savefig(newname)
-        break
-    plt.show()
+
+    for imatch in matching_arr:
+        plt.scatter(mod_x[imatch], mod_y[imatch], marker='X', s=100, color='r')
+        plt.text(mod_x[imatch], mod_y[imatch], mod_times[imatch])
 
 
-def plot1(berg_df, iceberg):
-    # t is the last timestep the model used
-    t = iceberg[0,-1]
-    t_all = iceberg[0,:]
-    nbt = berg_df.loc[abs(berg_df['hrs_since'] - t) == min(abs(berg_df['hrs_since'] -t))].index.values[0] - berg_df.index[0] # nearest berg time index from 0
-    plt.plot(berg_df['LONGITUDE'].values[:nbt+1], berg_df['LATITUDE'].values[:nbt+1], 
-             label='observed', marker='o') 
-    plt.plot(iceberg[1,:],iceberg[2,:], label='computed')
-    for k in range(nbt+1):
-        plt.annotate('{0:.1f}'.format(berg_df['hrs_since'].values[k]), 
-                     xy=(berg_df['LONGITUDE'].values[k],
-                         berg_df['LATITUDE'].values[k]))
-        tmp_diff = abs(t_all - berg_df['hrs_since'].values[k])
-        tmp_i = np.where(tmp_diff == tmp_diff.min())[0][0]
-        if tmp_i >= iceberg[0,:].size:
-            plt.annotate('{0:.1f}'.format(iceberg[0,-1]), 
-                     xy=(iceberg[1,-1],iceberg[2,-1]))
-            plt.plot(iceberg[1,-1],iceberg[2,-1], marker='o', color='black')
-        else:
-            plt.annotate('{0:.1f}'.format(t_all[tmp_i]), 
-                     xy=(iceberg[1,tmp_i],iceberg[2,tmp_i]))
-            plt.plot(iceberg[1,tmp_i],iceberg[2,tmp_i], marker='o', color='black')
 
-        
-    plt.plot(iceberg[1,:],iceberg[2,:], label='computed')
-    plt.xlabel('Longitude'); plt.ylabel('Latitude')
-    plt.title('Iceberg Drift\nSeason year: {}\nIIP indices: {}'.format(season_year, chosen_inds_arr[chosen_track_ind][:nbt+1]))
-    plt.savefig('./drift_track1.png')
-    plt.show()
-
-
-def plot2(berg_df, iceberg):
-    
-    obs_lats = berg_df['LATITUDE'].tolist()
-    obs_lons = berg_df['LONGITUDE'].tolist()
-    obs_times = berg_df['hrs_since'].tolist()
-    com_lats = iceberg[2,:]
-    com_lons = iceberg[1,:]
-    com_times = iceberg[0,:]
-    
-    # find the max time (in hours)
-    max_t = min(max(obs_times), max(com_times))
-    
-    # trim data to max time
-    obs_ind = bisect.bisect_right(obs_times, max_t)
-    obs_times = obs_times[:obs_ind+1]
-    obs_lats = obs_lats[:obs_ind+1]
-    obs_lons = obs_lons[:obs_ind+1]
-    
-    # find a bounding box in space
-    min_lat = min(min(obs_lats), min(com_lats))
-    max_lat = max(max(obs_lats), max(com_lats))
-    min_lon = min(min(obs_lons), min(com_lons))
-    max_lon = max(max(obs_lons), max(com_lons))
-    
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    buf = 0.1
-    ax.set_extent([min_lon-buf, max_lon+buf, min_lat-buf, max_lat+buf], ccrs.PlateCarree())
-    #ax.stock_img()
-    ax.coastlines('50m')
-    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                      linewidth=2, color='gray', alpha=0.5, linestyle='--')
-    
-    
-    gl.xlabels_top = False
-    gl.ylabels_left = False
-    gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER
-        
-    labels = [round(elem, 1) for elem in obs_times] 
-    for label, x, y in zip(labels, obs_lons, obs_lats):
-        ax.plot(x, y, marker='o', color='red')
-        ax.text(x, y, label)
-    
-    for i, i2 in enumerate(com_times):
-        for j in labels:
-            if abs(com_times[i] - j) < 0.001:
-                ax.plot(com_lons[i], com_lats[i], marker='X', color='green')
-                ax.text(com_lons[i], com_lats[i], j)
-    
-    ax.plot(obs_lons, obs_lats, color='red')
-    ax.plot(com_lons, com_lats, color='green')
-    
-    plt.savefig('./drift_track2.png')
-    plt.show()
-
-
-def plot3(berg_df, iceberg):
-
-    obs_lats = berg_df['LATITUDE'].tolist()
-    obs_lons = berg_df['LONGITUDE'].tolist()
-    obs_times = berg_df['hrs_since'].tolist()
-    com_lats = iceberg[2,:]
-    com_lons = iceberg[1,:]
-    com_times = iceberg[0,:]
-    
-    # find the max time (in hours)
-    max_t = min(max(obs_times), max(com_times))
-    
-    # trim data to max time
-    obs_ind = bisect.bisect_right(obs_times, max_t)
-    obs_times = obs_times[:obs_ind+1]
-    obs_lats = obs_lats[:obs_ind+1]
-    obs_lons = obs_lons[:obs_ind+1]
-    
-    # find a bounding box in space
-    min_lat = min(min(obs_lats), min(com_lats))
-    max_lat = max(max(obs_lats), max(com_lats))
-    min_lon = min(min(obs_lons), min(com_lons))
-    max_lon = max(max(obs_lons), max(com_lons))
-    
-    m = Basemap(llcrnrlon=min_lon-0.1, llcrnrlat=min_lat-0.1, urcrnrlon=max_lon+0.1, urcrnrlat=max_lat+0.1)
-    
-    m.drawmapboundary(fill_color='aqua')
-    m.fillcontinents(color='coral',lake_color='aqua')
-
+    # parallels are lines of latitude, meridians are lines of longitude
     # labels = [left,right,top,bottom]
-    parallels = np.arange(min_lat, max_lat, 0.1)
-    m.drawparallels(parallels,labels=[False,True,False,False])
-    meridians = np.arange(min_lon, max_lon, 0.1)
-    meridians = m.drawmeridians(meridians,labels=[False,False,True,False])
-    for mer in meridians:
-        try:
-            meridians[mer][1][0].set_rotation(45)
-        except:
-            pass
-    
-    obs_xpt, obs_ypt = m(obs_lons, obs_lats)
-    com_xpt, com_ypt = m(com_lons, com_lats)
-    m.plot(obs_xpt, obs_ypt, 'o')
-    m.plot(com_xpt, com_ypt, '.') 
-    
-    labels = [round(elem, 1) for elem in obs_times] 
-    for label, x, y in zip(labels, obs_xpt, obs_ypt):
-        plt.text(x, y, label)
-    
-    for i, i2 in enumerate(com_times):
-        for j in labels:
-            if abs(com_times[i] - j) < 0.001:
-                plt.plot(com_lons[i], com_lats[i], marker='X', color='black')
-                plt.text(com_lons[i], com_lats[i], j)
-    
-    #m.drawmapscale(lonn-buf/1.5, lat0+buf/5, lon0, lat0, length=100, barstyle='fancy')
-    plt.xlabel('Latitude')
-    plt.ylabel('Longitude')
-    plt.savefig('./drift_track3.png')
+    parallels = np.arange(berg_ymin, berg_ymax + buff, buff)
+    m.drawparallels(parallels,labels=[True,False,False,False])
+    meridians = np.arange(berg_xmin, berg_xmax + buff, buff)
+    m.drawmeridians(meridians,labels=[False,False,False,True])
+
     plt.show()
-
-
-
-def plot_return(iip_berg, mod_berg):
-    f = plt.figure()
-    tol = 0.1  # tstep
-    for i,t in enumerate(iip_berg.t2000):
-        diff = abs(np.asarray(mod_berg.t2000) - t)
-        for j,diff_t in enumerate(diff):
-            if diff_t < tol:
-                break
-        plt.plot(iip_berg.lons[i], iip_berg.lats[i], marker='o', color='red')
-        plt.text(iip_berg.lons[i], iip_berg.lats[i], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-        plt.plot(mod_berg.lons[j], mod_berg.lats[j], marker='o', color='yellow')
-        plt.text(mod_berg.lons[j], mod_berg.lats[j], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-    plt.plot(iip_berg.lons, iip_berg.lats, label='observed', color='red')
-    plt.plot(mod_berg.lons, mod_berg.lats, label='computed', color='orange')
-    plt.legend()
-    plt.xlabel('Longitude'); plt.ylabel('Latitude')
-    plt.title('Iceberg: {}\nStart time: {}'.format(iip_berg.id_num, iip_berg.times[0]))
-    return f
-
-
-def plot_return_size_vary(iip_berg, mod_berg_gr, mod_berg_bb, mod_berg_sm, mod_berg_med, 
-                         mod_berg_lg, mod_berg_vlg, ind=None):
-    f = plt.figure()
-    tol = 0.1  # tstep
-    for i,t in enumerate(iip_berg.t2000):
-        diff = abs(np.asarray(mod_berg_med.t2000) - t)
-        for j,diff_t in enumerate(diff):
-            if diff_t < tol:
-                break
-        plt.plot(iip_berg.lons[i], iip_berg.lats[i], marker='o', color='red')
-        plt.text(iip_berg.lons[i], iip_berg.lats[i], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-        plt.plot(mod_berg_med.lons[j], mod_berg_med.lats[j], marker='o', color='yellow')
-        plt.text(mod_berg_med.lons[j], mod_berg_med.lats[j], '{0:.1f}'.format(t-iip_berg.t2000[0]))
-    plt.plot(iip_berg.lons, iip_berg.lats, label='observed', color='red')
-    plt.plot(mod_berg_gr.lons, mod_berg_gr.lats, label='gr', color='orange')
-    plt.plot(mod_berg_bb.lons, mod_berg_bb.lats, label='bb', color='green')
-    plt.plot(mod_berg_sm.lons, mod_berg_sm.lats, label='sm', color='blue')
-    plt.plot(mod_berg_med.lons, mod_berg_med.lats, label='med', color='black')
-    plt.plot(mod_berg_lg.lons, mod_berg_lg.lats, label='lg', color='purple')
-    plt.plot(mod_berg_vlg.lons, mod_berg_vlg.lats, label='vlg', color='yellow')
-    plt.legend()
-    plt.xlabel('Longitude'); plt.ylabel('Latitude')
-    if ind is not None:
-        plt.title('Index: {}, Iceberg: {}\nStart time: {}'.format(ind,iip_berg.id_num, iip_berg.times[0]))
-    else:
-        plt.title('Iceberg: {}\nStart time: {}'.format(iip_berg.id_num, iip_berg.times[0]))
-    return f
-
-
-def plot_return_size_vary_no_time(iip_berg, mod_berg_gr, mod_berg_bb, mod_berg_sm, mod_berg_med, 
-                         mod_berg_lg, mod_berg_vlg, ind=None):
-    f = plt.figure()
-
-    plt.plot(iip_berg.lons, iip_berg.lats, label='observed', color='red')
-    plt.plot(mod_berg_gr.lons, mod_berg_gr.lats, label='gr', color='orange')
-    plt.plot(mod_berg_bb.lons, mod_berg_bb.lats, label='bb', color='green')
-    plt.plot(mod_berg_sm.lons, mod_berg_sm.lats, label='sm', color='blue')
-    plt.plot(mod_berg_med.lons, mod_berg_med.lats, label='med', color='black')
-    plt.plot(mod_berg_lg.lons, mod_berg_lg.lats, label='lg', color='purple')
-    plt.plot(mod_berg_vlg.lons, mod_berg_vlg.lats, label='vlg', color='yellow')
-    plt.legend()
-    plt.xlabel('Longitude'); plt.ylabel('Latitude')
-
-    return f
-
 
 
         
 def animate_currents(ocean_data, iip_berg, mod_berg):
+    """This function saves a gif of the ocean current velocities alongside an iceberg drift test case.
+    
+    Args:
+        ocean_data (ECMWF_Ocean): ocean data for time and space of iceberg drift simulation.
+        iip_berg (Iceberg): all attributes correspond to data from a particular IIP iceberg.
+        mod_berg (Iceberg): initial attributes and final time correspond to data from a particular 
+                            IIP iceberg but the rest comes from drift simulation.
+    """
+    
     
     mod_berg_t1950 = nc.date2num(mod_berg.datetimes, 
                                  'hours since 1950-01-01 00:00:00.0 00:00', 'standard')
@@ -319,9 +148,15 @@ def animate_currents(ocean_data, iip_berg, mod_berg):
         
     
     
-    
-    
 def animate_winds(atm_data, iip_berg, mod_berg):
+    """This function saves a gif of the wind velocities alongside an iceberg drift test case.
+    
+    Args:
+        atm_data (ECMWF_Atm): atmospheric data for time and space of iceberg drift simulation.
+        iip_berg (Iceberg): all attributes correspond to data from a particular IIP iceberg.
+        mod_berg (Iceberg): initial attributes and final time correspond to data from a particular 
+                            IIP iceberg but the rest comes from drift simulation.
+    """
     
     mod_berg_t1900 = nc.date2num(mod_berg.datetimes, 
                                  'hours since 1900-01-01 00:00:00.0 00:00', 'standard')
@@ -381,95 +216,3 @@ def animate_winds(atm_data, iip_berg, mod_berg):
     anim = FuncAnimation(fig, animate, frames=wind_mag[:,0,0].size-1, interval=100)
     #HTML(anim.to_html5_video())
     anim.save('plots/wind_mag.gif',writer='imagemagick')
-    
-
-def plot_turnbull(iip_berg, mod_berg):
-    
-    f = plt.figure()
-    
-    iip_times = np.asarray(iip_berg.datetimes)
-    mod_times = np.asarray(mod_berg.datetimes)
-    iip_t0 = iip_berg.datetimes[0]
-    mod_t0 = mod_berg.datetimes[0]
-    assert iip_t0 == mod_t0
-    iip_times0 = iip_times - iip_t0
-    mod_times0 = mod_times - mod_t0
-    iip_hours0, mod_hours0 = np.empty(0), np.empty(0)
-    
-    for i in range(len(iip_times0)):
-        iip_hours0 = np.append(iip_hours0, round(iip_times0[i].days*24 + iip_times0[i].seconds/3600, 1))
-        plt.text(iip_berg.lons[i], iip_berg.lats[i], '{}'.format(iip_hours0[i]))
-        
-    for j in range(len(mod_times0)):
-        mod_hours0 = np.append(mod_hours0,round(mod_times0[j].days*24 + mod_times0[j].seconds/3600, 1))
-        #plt.text(mod_berg.lons[j], mod_berg.lats[j], '{}H'.format(mod_hours0[j]))
-    ind_arr = []
-    tol = 1e-3
-    for k in range(len(iip_times0)):
-        mod_diff = mod_hours0 - iip_hours0[k]
-        #ind = np.where(mod_diff == 0.0)[0][0]
-        ind = np.where(abs(mod_diff < tol))[0][0]
-        ind_arr.append(ind)
-        plt.scatter(mod_berg.lons[ind], mod_berg.lats[ind], color='orange')
-        plt.text(mod_berg.lons[ind], mod_berg.lats[ind], '{}'.format(mod_hours0[ind]))
-        
-    
-        
-    plt.scatter(iip_berg.lons, iip_berg.lats, label='observed', color='red')
-    plt.plot(mod_berg.lons, mod_berg.lats, label='computed', color='orange')
-
-    plt.legend()
-    plt.xlabel('Longitude'); plt.ylabel('Latitude')
-
-    return f
-
-
-def plot_turnbull_subplots(iip_berg, mod_berg):
-    
-    f, (ax1, ax2) = plt.subplots(1,2)
-    f.tight_layout()
-    
-    iip_times = np.asarray(iip_berg.datetimes)
-    mod_times = np.asarray(mod_berg.datetimes)
-    iip_t0 = iip_berg.datetimes[0]
-    mod_t0 = mod_berg.datetimes[0]
-    assert iip_t0 == mod_t0
-    iip_times0 = iip_times - iip_t0
-    mod_times0 = mod_times - mod_t0
-    iip_hours0, mod_hours0 = np.empty(0), np.empty(0)
-    
-    for i in range(len(iip_times0)):
-        iip_hours0 = np.append(iip_hours0, round(iip_times0[i].days*24 + iip_times0[i].seconds/3600, 1))
-        ax1.text(iip_berg.lons[i], iip_berg.lats[i], '{}'.format(iip_hours0[i]))
-        
-    for j in range(len(mod_times0)):
-        mod_hours0 = np.append(mod_hours0,round(mod_times0[j].days*24 + mod_times0[j].seconds/3600, 1))
-        #plt.text(mod_berg.lons[j], mod_berg.lats[j], '{}H'.format(mod_hours0[j]))
-    ind_arr = []
-    for k in range(len(iip_times0)):
-        mod_diff = mod_hours0 - iip_hours0[k]
-        ind = np.where(mod_diff == 0.0)[0][0]
-        ind_arr.append(ind)
-        ax1.scatter(mod_berg.lons[ind], mod_berg.lats[ind], color='orange')
-        ax1.text(mod_berg.lons[ind], mod_berg.lats[ind], '{}'.format(mod_hours0[ind]))
-        
-    ax1.set_xlabel('Longitude') 
-    ax2.set_ylabel('Latitude')  
-    ax1.scatter(iip_berg.lons, iip_berg.lats, label='observed', color='red')
-    ax1.plot(mod_berg.lons, mod_berg.lats, label='computed', color='orange')
-
-    dists = []
-    dist_times = []
-    for l, item in enumerate(ind_arr):
-        dist = np.sqrt( (mod_berg.lons[item] - iip_berg.lons[l])**2 + \
-                       (mod_berg.lats[item] - iip_berg.lats[l])**2 )
-        dists.append(dist)
-        dist_time = mod_hours0[item]
-        dist_times.append(dist_time)
-    
-    ax2.set_ylabel('Distance (deg)')
-    ax2.set_xlabel('Time (hrs)')
-    ax2.set_ylim(0, max(dists) + np.mean(dists))
-    ax2.scatter(dist_times, dists)
-
-    return f
