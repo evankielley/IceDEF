dd74caef4aecd127c66e45025dfb5f28d1f35680
evankielley <evankielley@gmail.com>
2018-10-27 15:24:41 -0230

    fixed euler function and attempted rk2 function but right now it returns abnormally large values

--- icedef/timesteppers.py
+++ icedef/timesteppers.py
@@ -1,16 +1,29 @@
-def euler(f, h, x, **kwargs):
-    return x + h * f(v, **kwargs)
+import numpy as np
 
 
-def rk2(f, h, x, **kwargs):
-    k1 = h * f(x, **kwargs)
-    k2 = h * f(x + k1 / 2, **kwargs)
-    return x + k2
+def euler(f, h, *args, **kwargs):
+    return h * np.array(f(*args, **kwargs))
 
 
-def rk4(f, h, x, **kwargs):
-    k1 = h * f(x, **kwargs)
-    k2 = h * f(x + k1 / 2, **kwargs)
-    k3 = h * f(x + k2 / 2, **kwargs)
-    k4 = h * f(x + k3, **kwargs)
-    return x + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
+def rk2(f, h, *args, **kwargs):
+    """BROKEN - returns high values"""
+
+    k1 = h * np.array(f(*args, **kwargs))
+
+    args = np.array(args)
+    args[0] += np.timedelta64(int(h / 2), 's')
+
+    for i in range(1, len(args)):
+        args[i] += k1[i-1] / 2
+
+    args = tuple(args)
+
+    return h * np.array(f(*args, **kwargs))
+
+
+# def rk4(f, h, x, **kwargs):
+#     k1 = h * f(x, **kwargs)
+#     k2 = h * f(x + k1 / 2, **kwargs)
+#     k3 = h * f(x + k2 / 2, **kwargs)
+#     k4 = h * f(x + k3, **kwargs)
+#     return x + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
