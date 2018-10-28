f3d446351392bc03d7b3ea994a1a6ea33b1ef6c1
evankielley <evankielley@gmail.com>
2018-10-28 14:10:48 -0230

    added an Adams Moulton 2 method

--- icedef/timesteppers.py
+++ icedef/timesteppers.py
@@ -27,3 +27,26 @@ def rk2(f, h, *args, **kwargs):
 #     k3 = h * f(x + k2 / 2, **kwargs)
 #     k4 = h * f(x + k3, **kwargs)
 #     return x + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
+
+def am2(f, h, *args, **kwargs):
+
+    i = len(args[0]) - 1
+
+    if i == 0:
+        args0 = tuple([arg[0] for arg in args])
+        return h * np.array(f(*args0, **kwargs))
+    elif i == 1:
+        args0 = tuple([arg[0] for arg in args])
+        args1 = tuple([arg[1] for arg in args])
+        return h * (3 / 2 * np.array(f(*args1, **kwargs)) -
+                    1 / 2 * np.array(f(*args0, **kwargs)))
+    else:
+        args0 = tuple([arg[-3] for arg in args])
+        args1 = tuple([arg[-2] for arg in args])
+        args2 = tuple([arg[-1] for arg in args])
+        return h * (23 / 12 * np.array(f(*args2, **kwargs)) -
+                    4 / 3 * np.array(f(*args1, **kwargs)) +
+                    5 / 12 * np.array(f(*args0, **kwargs)))
+
+
+
