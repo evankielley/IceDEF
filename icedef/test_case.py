bb8c126ab4b540a7649c90f1f5ec70397abc195d
evankielley <evankielley@gmail.com>
2019-02-05 15:15:18 -0330

    for brevity, changed import statement of statoil-arcticnet data to be sd

--- icedef/test_case.py
+++ icedef/test_case.py
@@ -1,14 +1,14 @@
 import numpy as np
 import xarray as xr
-from icedef import statoil_arcticnet_data as statoil_data
+from icedef import statoil_arcticnet_data as sd
 
 
 class TestCase:
 
     def __init__(self, index_range=(1100, 1120)):
-        self.df = statoil_data.get_df(statoil_data.dir_path, statoil_data.csv_filenames[2])
+        self.df = sd.get_df(sd.dir_path + sd.csv_filenames[2])
         self.start_index, self.end_index = index_range
-        self.ref_berg = statoil_data.create_ref_berg_from_df(self.df, self.start_index, self.end_index)
+        self.ref_berg = sd.create_ref_berg_from_df(self.df, self.start_index, self.end_index)
         self.ref_times = self.ref_berg.history['time']
         self.ref_lats = xr.DataArray(self.ref_berg.history['latitude'], coords=[self.ref_times], dims=['time'])
         self.ref_lons = xr.DataArray(self.ref_berg.history['longitude'], coords=[self.ref_times], dims=['time'])
@@ -16,5 +16,4 @@ class TestCase:
         self.end_time = np.datetime64(self.df.DataDate_UTC[self.end_index])
         self.start_latitude = self.df.Latitude[self.start_index]
         self.start_longitude = self.df.Longitude[self.start_index]
-        self.start_velocity = statoil_data.get_iceberg_velocity_from_dataframe(
-            self.df, self.start_index, self.start_index + 10)
+        self.start_velocity = sd.get_iceberg_velocity_from_dataframe(self.df, self.start_index, self.start_index + 10)
