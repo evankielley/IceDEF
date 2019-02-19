92d8c8c55e6940b8d8b07a27aad18e39b1f15e92
evankielley <evankielley@gmail.com>
2019-02-19 18:07:55 -0330

    made a generic test case class and separated our typical curvy test case into its own subclass called TestCaseA

--- icedef/test_case.py
+++ icedef/test_case.py
@@ -5,17 +5,29 @@ from icedef import statoil_arcticnet_data as sd
 
 class TestCase:
 
-    def __init__(self, index_range=(1100, 1120)):
-        self.df = sd.get_df(sd.beacon_dir_path + sd.beacon_csv_filenames[2])
-        self.start_index, self.end_index = index_range
-        self.ref_berg = sd.create_ref_berg_from_df(self.df, self.start_index, self.end_index)
+    def __init__(self, beacon_id='204980', start_time=None, end_time=None):
+
+        self.df = sd.get_beacon_df(sd.beacon_dir_path + '0' + beacon_id + '0' + '_2015.csv', start_time=start_time, end_time=end_time)
+        self.ref_berg = sd.create_ref_berg_from_df(self.df)
         self.ref_times = self.ref_berg.history['time']
         self.ref_lats = xr.DataArray(self.ref_berg.history['latitude'], coords=[self.ref_times], dims=['time'])
         self.ref_lons = xr.DataArray(self.ref_berg.history['longitude'], coords=[self.ref_times], dims=['time'])
-        self.start_time = np.datetime64(self.df.DataDate_UTC[self.start_index])
-        self.end_time = np.datetime64(self.df.DataDate_UTC[self.end_index])
+        self.start_time = np.datetime64(self.df['DataDate_UTC'].values[0])
+        self.end_time = np.datetime64(self.df['DataDate_UTC'].values[len(self.df)-1])
         self.time_frame = self.start_time, self.end_time
-        self.start_latitude = self.df.Latitude[self.start_index]
-        self.start_longitude = self.df.Longitude[self.start_index]
+        self.start_latitude = self.df.Latitude[0]
+        self.start_longitude = self.df.Longitude[0]
         self.start_location = self.start_latitude, self.start_longitude
-        self.start_velocity = sd.get_iceberg_velocity_from_dataframe(self.df, self.start_index, self.start_index + 10)
+        self.start_velocity = sd.get_iceberg_velocity_from_dataframe(self.df, 0, 10)
+
+
+class TestCaseA(TestCase):
+
+    START_TIME = np.datetime64('2015-05-06T15:27:39')
+    END_TIME = np.datetime64('2015-05-07T06:25:51')
+    BEACON_ID = '90679'
+    BEACON_CSV_FILE_PATH = sd.beacon_dir_path + '0' + BEACON_ID + '_2015.csv'
+
+    def __init__(self):
+
+        super().__init__(beacon_id=self.BEACON_ID, start_time=self.START_TIME, end_time=self.END_TIME)
