a55504b75688c06a523f473dd9fd066895076ed2
evankielley <evankielley@gmail.com>
2019-02-19 23:30:37 -0330

    added docstrings

--- icedef/constants.py
+++ icedef/constants.py
@@ -1,13 +1,7 @@
-# units of density are: kilograms per meter cubed
-AIR_DENSITY = 1.027
-SEAWATER_DENSITY = 1025
-ICEBERG_DENSITY = 917
+"""This module contains constants that are used in calculations in other modules."""
 
-# units of drag form coefficients are: unit-less
-#ICEBERG_FORM_DRAG_COEFFICIENT_IN_AIR = 1.5
-#ICEBERG_FORM_DRAG_COEFFICIENT_IN_WATER = 1.5
-#ICEBERG_SKIN_DRAG_COEFFICIENT_IN_AIR = 2.5e-4
-#ICEBERG_SKIN_DRAG_COEFFICIENT_IN_WATER = 5e-4
-
+AIR_DENSITY = 1.027  # kilograms per meter cubed
+SEAWATER_DENSITY = 1025  # kilograms per meter cubed
+ICEBERG_DENSITY = 917  # kilograms per meter cubed
 EARTH_ROTATION_RATE = 7.2921e-5  # radians per second
 EARTH_RADIUS = 6378e3  # meters
